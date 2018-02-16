e9ed577497b3b152fb0e0167d8126b79a1e2e618
Ivan Dudarev <i.dudarev@itmegastar.com>
2018-02-16 15:52:26 +0700

    work with ddrv/extra-pack

--- src/Finder.php
+++ src/Finder.php
@@ -2,6 +2,8 @@
 
 namespace Ipstack\Finder;
 
+use Ddrv\Extra\Pack;
+
 /**
  * Class Finder
  *
@@ -40,63 +42,42 @@ class Finder
     protected $db;
 
     /**
-     * @var string Full data of database in memory
-     */
-    private $fulldata;
-
-    /**
-     * Mode in-memory is enabled or not
-     *
-     * @var bool
-     */
-    private $inMemory;
-
-    /**
      * Iptool constructor.
      *
      * @param string $databaseFile
-     * @param bool   $inMemory
      * @throws \InvalidArgumentException
      */
-    public function __construct($databaseFile, $inMemory = false)
+    public function __construct($databaseFile)
     {
         if (!is_readable($databaseFile)) {
             fclose($this->db);
             throw new \InvalidArgumentException('can not read database file');
         }
-        $this->inMemory = $inMemory;
         $this->fileSize = filesize($databaseFile);
         $this->db = fopen($databaseFile, 'rb');
 
-        if ($this->inMemory) {
-            $this->fulldata = $this->readFile(0, $this->fileSize);
-            fclose($this->db);
-            $this->db = null;
-        }
-
-        $meta = unpack('A3control/Ssize', $this->read(0, 5));
+        $meta = unpack('A3control/Ssize', fread($this->db, 5));
         if ($meta['control'] !== 'ISD') {
             fclose($this->db);
             throw new \InvalidArgumentException('file is not IPStack database');
         }
-
-        $header = $this->read(5, $meta['size']);
+        $header = fread($this->db, $meta['size']);
         $offset = 0;
-        $meta += unpack('Cversion/CRGC/SRGF/SRGD/CRLC/CRLF/SRLD', substr($header,$offset,10));
+        $meta += Pack::unpack('Cversion/ImaxItemLen/CRGC/SRGF/SRGD/CRLC/CRLF/SRLD', substr($header,$offset,14));
         if ($meta['version'] !== self::FORMAT_VERSION) {
             fclose($this->db);
             throw new \InvalidArgumentException('file is not IPStack database version '.self::FORMAT_VERSION);
         }
-
-        $offset += 10;
+        $this->meta['maxItemLen'] = $meta['maxItemLen'];
+        $offset += 14;
         $unpack = 'A'.$meta['RLF'].'RLUF/A'.$meta['RGF'].'RGMUF';
         $size = $meta['RLF']+$meta['RGF'];
-        $meta += unpack($unpack, substr($header, $offset, $size));
+        $meta += Pack::unpack($unpack, substr($header, $offset, $size));
 
         $this->meta['relations'] = array();
         $offset += $size;
         for ($i=0;$i<$meta['RLC'];$i++) {
-            $relation = unpack(
+            $relation = Pack::unpack(
                 $meta['RLUF'],
                 substr($header, $offset, $meta['RLD'])
             );
@@ -104,7 +85,7 @@ class Finder
             $offset += $meta['RLD'];
         }
         for ($i=0;$i<$meta['RGC'];$i++) {
-            $definion = unpack(
+            $definion = Pack::unpack(
                 $meta['RGMUF'],
                 substr($header, $offset, $meta['RGD'])
             );
@@ -113,7 +94,7 @@ class Finder
             $this->meta['registers'][$id] = $definion;
             $offset += $meta['RGD'];
         }
-        $definion = unpack(
+        $definion = Pack::unpack(
             $meta['RGMUF'],
             substr($header, $offset, $meta['RGD'])
         );
@@ -123,11 +104,8 @@ class Finder
         $this->meta['index'] = array_values(unpack('I*',substr($header, $offset)));
         $offset += 1029;
         $this->meta['networks']['offset'] = $offset;
-        $offset += $this->meta['networks']['items']*$this->meta['networks']['len'];
-
         foreach ($this->meta['registers'] as $id=>$register) {
-            $this->meta['registers'][$id]['offset'] = $offset;
-            $offset += ($register['items']+1)*$register['len'];
+            $this->meta['registers'][$id]['s'] += $offset;
         }
     }
 
@@ -150,21 +128,22 @@ class Finder
             $stop = $this->meta['index'][$octet];
         }
         if ($stop == $start) {
-            $stop = $this->meta['networks']['items'];
-        } elseif ($stop < $this->meta['networks']['items']) {
+            $stop = $this->meta['networks']['n'];
+        } elseif ($stop < $this->meta['networks']['n']) {
             $stop++;
         }
         if ($start > 0) {
             $start--;
         }
         $blockCount = $stop-$start;
-        $seek = $this->meta['networks']['offset']+($start*$this->meta['networks']['len']);
-        $blocks = $this->read($seek,$blockCount*$this->meta['networks']['len']);
+        $seek = $this->meta['networks']['offset']+($start*$this->meta['networks']['s']);
+        fseek($this->db,$seek);
+        $blocks = fread($this->db,$blockCount*$this->meta['networks']['s']);
         $start = 0;
         $stop = $blockCount;
         do {
             $center = ($start + $stop) >> 1;
-            $sc = substr($blocks, $center * $this->meta['networks']['len'], 4);
+            $sc = substr($blocks, $center * $this->meta['networks']['s'], 4);
             if ($sc > $long) {
                 $stop = $center;
             } else {
@@ -175,17 +154,17 @@ class Finder
         if ($long > $stop) {
             $start = $stop;
         }
-        $block = substr($blocks, $start * $this->meta['networks']['len'], $this->meta['networks']['len']);
-        $network = unpack('Nfirst',substr($block,0,4));
+        $block = substr($blocks, $start * $this->meta['networks']['s'], $this->meta['networks']['s']);
+        $network = Pack::unpack('Nfirst',substr($block,0,4));
         $data['network']['first'] = long2ip($network['first']);
-        $next = substr($blocks,($start+1)*$this->meta['networks']['len'],4);
+        $next = substr($blocks,($start+1)*$this->meta['networks']['s'],4);
         if ($next) {
-            $network = unpack('Nlast',$next);
+            $network = Pack::unpack('Nlast',$next);
             $data['network']['last'] = long2ip($network['last']-1);
         } else {
             $data['network']['last'] = '255.255.255.255';
         }
-        $registers = unpack($this->meta['networks']['pack'],substr($block,4));
+        $registers = Pack::unpack($this->meta['networks']['format'],substr($block,4));
         foreach ($registers as $register=>$item) {
             $data['data'][$register] = $this->getRegisterRecord($register,$item);
         }
@@ -193,6 +172,7 @@ class Finder
             foreach ($relations as $field=>$child) {
                 if (isset($data['data'][$parent][$field])) {
                     $data['data'][$child] = $this->getRegisterRecord($child, $data['data'][$parent][$field]);
+                    unset($data['data'][$parent][$field]);
                 }
             }
         }
@@ -201,9 +181,9 @@ class Finder
 
     public function getRegisterRecord($register,$item)
     {
-        if ($item > $this->meta['registers'][$register]['items']) $item = 0;
-        $seek = $this->meta['registers'][$register]['offset']+($item * $this->meta['registers'][$register]['len']);
-        $data = unpack($this->meta['registers'][$register]['pack'], $this->read($seek, $this->meta['registers'][$register]['len']));
+        $seek = $this->meta['registers'][$register]['s']+$item;
+        fseek($this->db,$seek);
+        $data = Pack::unpack($this->meta['registers'][$register]['format'],fread($this->db,$this->meta['maxItemLen']));
         return $data;
     }
 
@@ -214,17 +194,31 @@ class Finder
             'author' => '',
             'license' => '',
             'networks' => array(
-                'count' => $this->meta['networks']['items'],
+                'count' => $this->meta['networks']['n'],
                 'data' => array(),
             ),
         );
-        $register = array('offset'=>0,'len'=>0,'items'=>0);
+
         foreach ($this->meta['registers'] as $r=>$register) {
-            $about['networks']['data'][$r] = array_keys(unpack($register['pack'],str_pad('',$register['len'],' ')));
+            $about['networks']['data'][$r] = array_keys(
+                Pack::unpack(
+                    $register['format'],
+                    str_pad('', $this->meta['maxItemLen'],"\0")
+                )
+            );
         }
-        $offset = $register['offset'] + ($register['len'] * ($register['items']+1));
-        $info = $this->read($offset,$this->fileSize - $offset);
-        $tmp = unpack('N1created/A128author/A*license', $info);
+
+        $offset = $this->meta['networks']['offset'];
+        $offset += $this->meta['networks']['s']*$this->meta['networks']['n'];
+        foreach ($this->meta['registers'] as $register=>$data) {
+            $offset += $data['n'];
+        }
+
+        fseek($this->db,$offset);
+
+        $info = fread($this->db,$this->fileSize-$offset);
+
+        $tmp = Pack::unpack('Icreated/~author/~license', $info."\0");
         $about = array_replace($about, $tmp);
         return $about;
     }
@@ -239,47 +233,14 @@ class Finder
     {
         if (!isset($this->meta['registers'][$register])) return array();
         $result = array();
-        $seek = $this->meta['registers'][$register]['offset']+$this->meta['registers'][$register]['len'];
-        if (!$this->inMemory) {
-            fseek($this->db,$seek);
-        }
-        for($i = 1;$i<=$this->meta['registers'][$register]['items'];$i++) {
-            $length = $this->meta['registers'][$register]['len'];
-            if ($this->inMemory) {
-                $data = $this->readMemory($seek, $length);
-                $seek += $length;
-            } else {
-                $data = $this->readFile(null, $length);//we do not use $this->read to not call fseek in cycle
-            }
-            $result[$i] = unpack($this->meta['registers'][$register]['pack'], $data);
-        }
+        $num = 0;
+        do {
+            $item = $this->getRegisterRecord($register, $num);
+            $result[$num] = $item;
+            $bin = Pack::pack($this->meta['registers'][$register]['format'], $item);
+            $len = strlen($bin);
+            $num += $len;
+        } while ($len && $num < $this->meta['registers'][$register]['n']);
         return $result;
-    }
-
-    private function read($offset, $length)
-    {
-        if ($this->inMemory) {
-            return $this->readMemory($offset, $length);
-        }
-
-        return $this->readFile($offset, $length);
-    }
-
-    private function readFile($offset, $length)
-    {
-        if ($offset !== null) {
-            fseek($this->db, $offset);
-        }
-
-        return fread($this->db, $length);
-    }
-
-    private function readMemory($offset, $length)
-    {
-        if ($this->fulldata === null || $this->fulldata === '') {
-            throw new \RuntimeException('Database not loaded');
-        }
-
-        return substr($this->fulldata, $offset, $length);
     }
 }
